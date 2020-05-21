2d19df4e2fdf0bf7a8b0f742cd49441c9f6cd155
诗人的咸鱼 <willnewii@163.com>
2020-05-21 20:20:23 +0800

    fix：小程序组件自动保存图片时添加延时

--- example/weapp/component/json2canvas/index.js
+++ example/weapp/component/json2canvas/index.js
@@ -13,79 +13,84 @@ Component({
     showCanvas: {
       type: Boolean,
       value: true,
-    }
+    },
   },
   observers: {
-    'painting': function (value) {
+    painting: function(value) {
       if (!this.data.isPainting) {
         if (JSON.stringify(value)) {
           if (value && value.width && value.height) {
             this.setData({
-              isPainting: true
-            })
-            this.readyPigment()
+              isPainting: true,
+            });
+            this.readyPigment();
           }
         }
       }
-    }
+    },
   },
   data: {
-    caxId: 'caxId-' + new Date().getTime(),
-    canvasId: '',
+    caxId: "caxId-" + new Date().getTime(),
+    canvasId: "",
     width: 100,
     height: 100,
-    isPainting: false
+    isPainting: false,
   },
   ctx: null,
   lifetimes: {
     ready() {
       const caxComponent = this.getCaxComponent();
       this.setData({
-        canvasId: caxComponent.data.id
-      })
-    }
+        canvasId: caxComponent.data.id,
+      });
+    },
   },
   methods: {
     getCaxComponent() {
-      return this.selectComponent('#' + this.data.caxId);
+      return this.selectComponent("#" + this.data.caxId);
     },
     readyPigment() {
       const inter = setInterval(() => {
         if (this.data.canvasId) {
-          clearInterval(inter)
+          clearInterval(inter);
           this.setData({
             isPainting: true,
-          })
+          });
 
           json2canvas.draw(this.data.painting, this.data.caxId, this, () => {
             this.setData({
               isPainting: false,
-            })
-            this.data.saveImage && this.saveImageToLocal();
+            });
+            setTimeout(() => {
+              this.data.saveImage && this.saveImageToLocal();
+            }, 500);
           });
         }
-      }, 100)
+      }, 100);
     },
     saveImageToLocal() {
       const { width, height, scale } = this.data.painting;
 
-      wx.canvasToTempFilePath({
-        x: 0,
-        y: 0,
-        width: width * scale,
-        height: height * scale,
-        canvasId: this.data.canvasId,
-        complete: res => {
-          if (res.errMsg === 'canvasToTempFilePath:ok') {
-            this.setData({
-              isPainting: false,
-            })
-            this.triggerEvent('getImage', { tempFilePath: res.tempFilePath, errMsg: 'canvasdrawer:ok' })
-          } else {
-            this.triggerEvent('getImage', { errMsg: 'canvasdrawer:fail' })
-          }
-        }
-      }, this.getCaxComponent())
-    }
-  }
-})
+      wx.canvasToTempFilePath(
+        {
+          x: 0,
+          y: 0,
+          width: width * scale,
+          height: height * scale,
+          canvasId: this.data.canvasId,
+          complete: (res) => {
+            if (res.errMsg === "canvasToTempFilePath:ok") {
+              this.setData({
+                isPainting: false,
+              });
+              this.triggerEvent("getImage", { tempFilePath: res.tempFilePath, errMsg: "canvasdrawer:ok" });
+            } else {
+              this.triggerEvent("getImage", { errMsg: "canvasdrawer:fail" });
+            }
+          },
+        },
+        this.getCaxComponent()
+      );
+    },
+  },
+});
